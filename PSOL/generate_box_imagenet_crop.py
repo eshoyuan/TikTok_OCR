a39fe012073a42363cd2de19c7ad598f0f93fb0d
Yixiao Yuan <eshoyuan@gmail.com>
2022-08-24 11:51:56 +0800

    update PSOL code

--- PSOL/generate_box_imagenet_crop.py
+++ PSOL/generate_box_imagenet_crop.py
@@ -23,9 +23,9 @@ from loader.ddt_imagenet_dataset import DDTImageNetDataset
 parser = argparse.ArgumentParser(description='Parameters for DDT generate box')
 parser.add_argument('--input_size',default=448,dest='input_size')
 parser.add_argument('--data',default="data/PSOL_imgs",help='path to imagenet dataset')
-parser.add_argument('--gpu',help='which gpu to use',default='0,1,2,3',dest='gpu')
+parser.add_argument('--gpu',help='which gpu to use',default='0',dest='gpu')
 parser.add_argument('--output_path',default='data/DDT_crop/',dest='output_path')
-parser.add_argument('--batch_size',default=64,dest='batch_size')
+parser.add_argument('--batch_size',default=32,dest='batch_size')
 args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 os.environ['OMP_NUM_THREADS'] = "10"
